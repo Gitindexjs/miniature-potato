447b1056a2cf355e1eff6c7d0f899b8aeb60722f
Gitindexjs <happypor10@gmail.com>
2022-06-19 22:39:59 +0100

    beginnings

--- Assets/scripts/MothMovement.cs
+++ Assets/scripts/MothMovement.cs
@@ -1,20 +1,36 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MothMovement : MonoBehaviour
 {
-	System.Random rand;
-    // Start is called before the first frame update
-    void Start()
-    {
-		rand = new System.Random();
-    }
+	[SerializeField] NavMeshAgent agent;
+
+	Vector3 nextLocation;
+	bool nextLocationCreated;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(rand.Next(100)/1000 - 50, rand.Next(100)/1000 - 50, 0);
+        if(!nextLocationCreated) findNextLocation();
+		if(nextLocationCreated) 
+			agent.SetDestination(nextLocation);
+
+		Vector3 distanceToNextLocation = transform.position - nextLocation;
+
+		if(distanceToNextLocation.magnitude < 0.2f) {
+			nextLocationCreated = false;
+		}
     }
+
+	void findNextLocation() {
+		float randomX = Random.Range(-2f, 2f);
+		float randomY = Random.Range(-2f, 2f);
+
+		nextLocation = new Vector3(transform.position.x + randomX, transform.position.y + randomY, transform.position.z);
+
+		nextLocationCreated = true;
+
+	}
 }
